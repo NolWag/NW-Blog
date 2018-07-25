467ccc0938390af849ef62990cecb65621c7926b
NolWag <NolanDubyu@gmail.com>
2018-07-25 13:40:27 -0500

    fir

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: 'Dev Blog', // Required
+  author: 'Nolan Wagner', // Required
+  tagline: 'Full-stack Web Developer',
+  primaryColor: '#3498db', // Required
+  showHeaderImage: true,
+  showShareButtons: true,
+  postsPerPage: 5, // Required
+  social: {
+    website: 'https://68c7ea62b7f24e9692ecfdfa2aec07a4.codepen.website/#',
+    github: 'https://github.com/nolwag',
+    twitter: 'https://twitter.com/',
+    linkedin: 'https://ca.linkedin.com/'
+  }
+};
